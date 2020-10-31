7b5345b7178649bd54d876acd50e58971d5795d2
libicap <rebeccapsinhui@gmail.com>
2020-10-31 13:40:56 +0200

    Start change font

--- ModTemplate/ChangeFont.cs
+++ ModTemplate/ChangeFont.cs
@@ -4,10 +4,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace ModTemplate
 {
-    public class ModTemplate : ModBehaviour
+    public class ChangeFont : ModBehaviour
     {
         private void Awake()
         {
@@ -19,7 +21,15 @@ namespace ModTemplate
         private void Start()
         {
             // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(ModTemplate)} is loaded!", MessageType.Success);
+            ModHelper.Console.WriteLine($"My mod {nameof(ChangeFont)} is loaded!", MessageType.Success);
+
+            var font = Font.CreateDynamicFontFromOSFont("AR BLANCA", 10);
+
+            var texts = FindObjectsOfType<Text>();
+            foreach (var text in texts)
+            {
+                text.font = font;
+            }
         }
     }
 }
